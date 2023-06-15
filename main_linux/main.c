b7a2bde96a52190a6feb0fb35fcd620947cd9018
frostyDesu <72258405+frostyDesu@users.noreply.github.com>
2023-06-15 00:47:01 +0000

    menu

--- main_linux/main.c
+++ main_linux/main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <unistd.h>
-
-//Cores
 #define G "\x1b[32m"
 #define Y "\x1b[33m"
 #define R "\x1b[31m"
@@ -11,92 +8,138 @@
 #define E "\x1b[0m"
 
 int main() {
-  
-int opcao = 0;
-  while(1) {
+
+  int opcao = 0;
+  while (1) {
     system("clear");
     usleep(55000);
-printf(G "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"E"\n");
+    printf(G "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"
+             "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄" E "\n");
     usleep(55000);
-printf(G "█                                                                               █"E"\n");
+    printf(G "█                                                                "
+             "               █" E "\n");
     usleep(55000);
-printf(G " █           ▄█▀▀▀█▄█                         ▀███                             █"E"\n");
+    printf(G " █           ▄█▀▀▀█▄█                         ▀███               "
+             "              █" E "\n");
     usleep(55000);
-printf(G "█           ▄██    ▀█                           ██                              █"E"\n"); 
+    printf(G "█           ▄██    ▀█                           ██               "
+             "               █" E "\n");
     usleep(55000);
-printf(G " █           ▀███▄     ▀████████▄     ▄█▀██▄    ██  ▄██▀    ▄▄█▀▀█▄            █"E"\n");
+    printf(G " █           ▀███▄     ▀████████▄     ▄█▀██▄    ██  ▄██▀    "
+             "▄▄█▀▀█▄            █" E "\n");
     usleep(55000);
-printf(G "█              ▀█████▄   ██    ██    ██   ██    ██ ▄█      ▄█▀   ██             █"E"\n");
+    printf(G "█              ▀█████▄   ██    ██    ██   ██    ██ ▄█      ▄█▀   "
+             "██             █" E "\n");
     usleep(55000);
-printf(G " █           ▄     ▀██   ██    ██     ▄█████    ██▄██      ██▀▀▀▀▀▀            █"E"\n");
+    printf(G " █           ▄     ▀██   ██    ██     ▄█████    ██▄██      "
+             "██▀▀▀▀▀▀            █" E "\n");
     usleep(55000);
-printf(G "█            ██     ██   ██    ██    ██   ██    ██ ▀██▄    ██▄   ▄▄             █"E"\n");
+    printf(G "█            ██     ██   ██    ██    ██   ██    ██ ▀██▄    ██▄   "
+             "▄▄             █" E "\n");
     usleep(55000);
-printf(G " █           ▀██████▀  ▄████  ████  ▄████▀██▄  ████▄ ██▄▄  ▀██████▀            █"E"\n");
+    printf(G " █           ▀██████▀  ▄████  ████  ▄████▀██▄  ████▄ ██▄▄  "
+             "▀██████▀            █" E "\n");
     usleep(55000);
-printf(G "█                                                                               █"E"\n"); 
+    printf(G "█                                                                "
+             "               █" E "\n");
     usleep(55000);
-printf(G "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"E"\n");
+    printf(G "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+             "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀" E "\n");
+
+    printf("                                                                   "
+           "          \n");
+
+    printf("                                                                   "
+           "          \n");
+    usleep(56000);
+    printf(Y "                                1 - Novo jogo                    "
+             "            " E "\n");
+    printf("                                                                   "
+           "          \n");
+    printf(Y "                                2 - Como Jogar                   "
+             "            " E "\n");
+    printf("                                                                   "
+           "          \n");
+    printf(Y "                                3 - Creditos                     "
+             "            " E "\n");
+    printf("                                                                   "
+           "          \n");
     
-    printf("                                                                             \n");
-    usleep(356000);
-    printf(Y"                                1 - Novo jogo                                "E"\n");
-    printf("                                                                             \n");
-    printf(Y"                                2 - Como Jogar                               "E"\n");
-    printf("                                                                             \n");
-    printf(Y"                                3 - Creditos                                 "E"\n");
-    printf("                                                                             \n");
-    printf(R"                                0 - Sair do jogo                             "E"\n");
+    //consertar as cores que estão dando erro de compilação.
+    
+    printf( /*R*/ "                           0 - Sair do jogo                  " /*E*/
+        "\n"); 
+
     scanf("%i", &opcao);
     getchar();
 
-   switch(opcao){
-     case 1: {
-     //jogo inicia aqui
-       break; }
-     
-     case 2: {
-       
-       printf("1 - controles");
-       case 4: {
-         
-       printf("\n");
-       printf("Controles\n");
-       printf("\n");
-       usleep(55000);
-       printf("W - Para cima\n");
-       usleep(55000);
-       printf("S - Para baixo\n");
-       usleep(55000);
-       printf("D - Para a direita\n");
-       usleep(55000);
-       printf("A - Para a esquerda\n"); 
-       printf("\n");
-       break; }
-         
-       break; }
+    switch (opcao) {
+    case 1: {
+      system("clear");
+      // jogo inicia aqui
+      break;
+    }
+
+    case 2: {
+      while (1) {
+              
+      system("clear");
+      printf("1 - Controles\n");
+      printf("2 - Regras\n");
+      printf("0 - Voltar\n");
+        
+      scanf("%i", &opcao);
+      getchar();
+      switch (opcao) {
+
+      case 1:
+        printf("\n");
+        printf("Controles\n");
+        printf("\n");
+        usleep(55000);
+        printf("W - Para cima\n");
+        usleep(55000);
+        printf("S - Para baixo\n");
+        usleep(55000);
+        printf("D - Para a direita\n");
+        usleep(55000);
+        printf("A - Para a esquerda\n");
+        printf("\n");
+        break;
+
+      case 2:
+        printf("regras\n");
+        break;
+      }
+
+      break;
+    }
+      }
 
 
-     case 3: {
-       printf("\n");
-       printf(G"Jogo Snake - versão 1.0 early"E"\n");
-       printf(Y"Language C edition!"E"\n");
-       printf("\n");
-       printf(B"Feito por Davi e Rafael"E"\n");
-       printf("\n"); 
-    break; }
-     
-     case 0:
-     break;
-     default: printf(R"Opção inválida!!"E"\n");
-   } 
+    case 3: {
+      system("clear");
+      printf("\n");
+      printf(G "Jogo Snake - versão 1.0 early" E "\n");
+      printf(Y "Language C edition!" E "\n");
+      printf("\n");
+      printf(B "Feito por Davi e Rafael" E "\n");
+      printf("\n");
+      break;
+    }
 
-    printf(G"Precione enter para continuar.." E "\n");
+    case 0:
+      break;
+    default:
+      printf(/*R*/"Opção inválida!!"/*E*/ "\n");
+    }
+
+    printf(G "Precione enter para continuar.." E "\n");
     getchar();
 
     if (opcao == 0) {
       break;
-    } 
+    }
   }
   return 0;
 }
